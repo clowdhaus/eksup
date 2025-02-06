9801cbf458e9ee34aa571bbc4c0fbc3e31ee0652
Bryant Biggs <bryantbiggs@gmail.com>
2025-02-06 07:54:50 -0600

    feat: Add support for Kubernetes `1.32`

--- eksup/src/version.rs
+++ eksup/src/version.rs
@@ -6,7 +6,7 @@ use seq_macro::seq;
 use serde::{Deserialize, Serialize};
 
 /// Latest support version
-pub const LATEST: &str = "1.31";
+pub const LATEST: &str = "1.32";
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Versions {
@@ -14,7 +14,7 @@ pub struct Versions {
   pub target: String,
 }
 
-seq!(N in 26..=31 {
+seq!(N in 26..=32 {
     /// Kubernetes version(s) supported
     #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
     pub enum KubernetesVersion {
