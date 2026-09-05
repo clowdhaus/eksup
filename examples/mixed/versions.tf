f78fa6b1a73df936442eddf0cffcaae3857ab4b8
Bryant Biggs <bryantbiggs@gmail.com>
2026-09-05 08:00:19 -0500

    chore(deps): update all dependencies to latest

--- examples/mixed/versions.tf
+++ examples/mixed/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.55"
+      version = ">= 6.63"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
